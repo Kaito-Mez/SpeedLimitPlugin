4bf26ab6bf41d1a034e1e31d289329221863544f
Kaito-Mez <jameskm721@gmail.com>
2023-04-29 10:13:47 +1000

    Initial Commit

---
+++ EntryCarSpeedManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using AssettoServer.Network.Packets.Incoming;
+using AssettoServer.Network.Packets.Outgoing;
+using AssettoServer.Network.Packets.Shared;
+using AssettoServer.Network.Tcp;
+using AssettoServer.Server;
+
+namespace SpeedLimitPlugin;
+
+public class EntryCarSpeedManager
+{
+    private readonly EntryCar _entryCar;
+    
+
+    public EntryCarSpeedManager(EntryCar entryCar)
+    {
+        _entryCar = entryCar;
+        _entryCar.PositionUpdateReceived += OnPositionUpdateReceived;
+    }
+
+
+    private void OnPositionUpdateReceived(EntryCar sender, in PositionUpdateIn positionUpdate)
+    {
+    }
+
+}
---
+++ SpeedLimitPlugin.cs
@@ -0,0 +1,31 @@
+﻿using AssettoServer.Server;
+using AssettoServer.Server.Plugin;
+using AssettoServer.Utils;
+using Microsoft.Extensions.Hosting;
+
+namespace SpeedLimitPlugin;
+
+public class SpeedLimitPlugin : CriticalBackgroundService, IAssettoServerAutostart
+{
+    private readonly EntryCarManager _entryCarManager;
+    private readonly Func<EntryCar, EntryCarSpeedManager> _entryCarRaceFactory;
+    private readonly Dictionary<int, EntryCarSpeedManager> _instances = new();
+    
+    public SpeedLimitPlugin(EntryCarManager entryCarManager, Func<EntryCar, EntryCarSpeedManager> entryCarRaceFactory, IHostApplicationLifetime applicationLifetime) : base(applicationLifetime)
+    {
+        _entryCarManager = entryCarManager;
+        _entryCarRaceFactory = entryCarRaceFactory;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        foreach (var entryCar in _entryCarManager.EntryCars)
+        {
+            _instances.Add(entryCar.SessionId, _entryCarRaceFactory(entryCar));
+        }
+
+        return Task.CompletedTask;
+    }
+    
+    internal EntryCarSpeedManager GetRace(EntryCar entryCar) => _instances[entryCar.SessionId];
+}
